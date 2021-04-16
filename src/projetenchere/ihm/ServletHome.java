172211e0116db9f7accbcc4c8b33b8bee8cdd82c
william <w.pennaneach@gmail.com>
2021-04-16 11:51:27 +0200

    affichage meilleure offre

--- src/projetenchere/ihm/ServletHome.java
+++ src/projetenchere/ihm/ServletHome.java
@@ -1,7 +1,10 @@
 package projetenchere.ihm;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -27,15 +30,27 @@ public class ServletHome extends HttpServlet {
 		ManagerEnchere managerEnchere = ManagerSingleton.getManagerEnchere();
 		ManagerCategorie managerCategorie = ManagerSingleton.getManagerCategorie();
 
-		List<Enchere> encheres = managerEnchere.GetAllEnchere();
+		List<Enchere> lesEncheres = managerEnchere.GetAllEnchere();
+		List<Enchere> encheres = new ArrayList();
+		List<Integer> numEncheres = new ArrayList();
+		
+		for(Enchere enchere : lesEncheres) {
+			if(numEncheres.stream().filter(x -> x.intValue() == enchere.getArticleVendu().getNoArticle()).collect(Collectors.toList()).size() == 0) {
+				Enchere meilleureEnchere = managerEnchere.GetMeilleureOffre(enchere.getArticleVendu());
+				encheres.add(meilleureEnchere);
+				numEncheres.add(enchere.getArticleVendu().getNoArticle());
+			}
+			
+		}
+		
+		for(Enchere lenchere : encheres) {
+			System.out.println(lenchere);
+		}
+		
 		List<Categorie> categories = managerCategorie.GetAllCategorie();
 		
 		request.setAttribute("categories", categories);
 		request.setAttribute("encheres", encheres);
-
-		for (Enchere ench: encheres){
-			System.out.println(ench.toString());
-		}
 		
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/pages/home.jsp");
 		rd.forward(request, response);
